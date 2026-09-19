f4112e59eb1888b8027fbbc24449b3a2dd2e10e3
= <=>
2026-09-19 23:34:10 +0300

    cors()

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -4,9 +4,11 @@ import { connectToDataBase } from './database/connection.js';
 import { globalErrorHandling } from './common/middleware/error.middleware.js';
 import authRouter from './module/auth/auth.controller.js';
 import roomsRouter from './module/rooms/room.controller.js';
+import cors from 'cors';
+import morgan from 'morgan';
 const app = express();
 function bootstrap() {
-    app.use(express.json());
+    app.use(express.json(), cors(), morgan('combined'));
     connectToDataBase();
     app.use('/auth', authRouter);
     app.use('/rooms', roomsRouter);
